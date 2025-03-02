15e8d7bc5d7039b602b934ec06d21d1588b72c88
pcampina <pablo@campina.dev>
2025-03-02 23:06:39 +0000

    Repositories, Dependency inversion principle and use-cases

--- aulas/03-api-solid/src/app.ts
+++ aulas/03-api-solid/src/app.ts
@@ -1,13 +1,24 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.register(appRoutes)
 
-prisma.user.create({
-  data: {
-    name: 'Pablo Campina',
-    email: 'pablo@campina.dev',
-  },
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'prod') {
+    console.error(error)
+  } else {
+    // TODO: we should monitor the errors on some tool like Datadog/Newrelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
 })
---
+++ aulas/03-api-solid/src/http/controllers/register.controller.ts
@@ -0,0 +1,34 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users.repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists.error'
+import { RegisterUseCase } from '@/use-cases/register'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+
+    return reply.status(500).send() // TODO: fix generic error
+  }
+
+  return reply.status(201).send()
+}
