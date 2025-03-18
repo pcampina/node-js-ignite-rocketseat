c89b00f116cefeefd550af5e496e348d52d09e87
pcampina <pablo@campina.dev>
2025-03-18 22:04:12 +0000

    controllers organization improvement, created check-ins and gyms controllers

---
+++ aulas/03-api-solid/src/http/controllers/users/routes.ts
@@ -0,0 +1,13 @@
+import { FastifyInstance } from 'fastify'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { register } from './register.controller'
+import { authenticate } from './authenticate.controller'
+import { profile } from './profile.controller'
+
+export async function userRoutes(app: FastifyInstance) {
+  app.post('/users', register)
+  app.post('/sessions', authenticate)
+
+  // Authenticated
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
+}
