a9a1c6cbe1bd13fd521ba548bf99eb7521cc2f20
pcampina <pablo@campina.dev>
2025-03-21 09:05:09 +0000

    refresh jwt token

--- aulas/03-api-solid/src/http/controllers/users/routes.ts
+++ aulas/03-api-solid/src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { register } from './register.controller'
 import { authenticate } from './authenticate.controller'
 import { profile } from './profile.controller'
+import { refresh } from './refresh.controller'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   // Authenticated
   app.get('/me', { onRequest: [verifyJWT] }, profile)
